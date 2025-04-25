a85e94e1b6a3799c86e58150d5d35b5ed98e6721
Patrick Gephart <pmgephart@gmail.com>
2025-04-25 09:57:55 -0600

    removed unused util file

--- src/util/activities.tsx
+++
@@ -1,6 +0,0 @@
-export function getActivites() {
-    const response = await fetch(`http://localhost:3000/api/activities`);
-    const activities = await response.json();
-
-    return activities;
-}
